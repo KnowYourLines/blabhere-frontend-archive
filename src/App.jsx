932d86077d2ac60ee1b86b8a557ba8e75b059303
Johnny <johnkcli@hotmail.com>
2024-10-03 21:21:24 +0100

    get or generate new room based on url

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-import Copyright from './Copyright';
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ProTip from "./ProTip";
+import Copyright from "./Copyright";
 
 export default function App() {
+  const [room, setRoom] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const currentRoom = urlParams.get("room");
+    if (!currentRoom) {
+      const newRoom = uuidv4();
+      const url = new URL(window.location.href.split("?")[0]);
+      url.searchParams.set("room", newRoom);
+      window.history.replaceState("", "", url);
+      const newUrlParams = new URLSearchParams(window.location.search);
+      setRoom(newUrlParams.get("room"));
+    } else {
+      setRoom(currentRoom);
+    }
+  }, [room]);
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
