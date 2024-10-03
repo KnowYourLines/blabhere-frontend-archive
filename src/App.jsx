de838d8bd5377ef45c1e010ffbe1ac645763cb49
Johnny <johnkcli@hotmail.com>
2024-10-03 21:55:24 +0100

    authenticates with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
@@ -8,6 +10,29 @@ import Copyright from "./Copyright";
 
 export default function App() {
   const [room, setRoom] = useState("");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyD6HWYS1hbYXR7xvKgq7hQW-T4wSECWnss",
+      authDomain: "blabhere-29279.firebaseapp.com",
+      projectId: "blabhere-29279",
+      storageBucket: "blabhere-29279.appspot.com",
+      messagingSenderId: "304567083706",
+      appId: "1:304567083706:web:25db98d0c4edb2326e9883",
+      measurementId: "G-V5JWXF119M",
+    };
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const token = await user.getIdToken();
+        setToken(token);
+      } else {
+        signInAnonymously(auth);
+      }
+    });
+  }, [token]);
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const currentRoom = urlParams.get("room");
